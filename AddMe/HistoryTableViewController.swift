d9628faacfd978f2b974217dbd3e631c7a84fbf1
justin.lee2369@gmail.com <justin.lee2369@gmail.com>
2016-06-06 19:34:44 -0700

    added History VC + iPhone UI changes

---
+++ AddMe/HistoryTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  HistoryTableViewController.swift
+//  AddMe
+//
+//  Created by Justin Lee on 6/6/16.
+//  Copyright © 2016 Justin Lee. All rights reserved.
+//
+
+import Foundation
+
+class HistoryTableViewController: UITableViewController {
+    
+}
