6e74a2751852e084c73a7568a0694eed46f88f64
justin.lee2369@gmail.com <justin.lee2369@gmail.com>
2016-06-07 05:07:52 -0700

    history file included

--- AddMe/HistoryTableViewController.swift
+++ AddMe/HistoryTableViewController.swift
@@ -7,14 +7,19 @@
 //
 
 import Foundation
+import CoreData
 
 class HistoryTableViewController: UITableViewController {
     
-
+    var managedObjectContext: NSManagedObjectContext? =
+        (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext
     var data: [String] = []
     
     // MARK: - UITableViewDataSource
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        retrieveCurrentUserDetails()
+    }
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return data.count
     }
@@ -24,5 +29,24 @@ class HistoryTableViewController: UITableViewController {
         cell.textLabel?.text = data[indexPath.row]
         return cell
     }
+    
+    private func retrieveCurrentUserDetails() -> [String] {
+        managedObjectContext?.performBlock(
+            {
+                // Get the User from Core Data
+                do {
+                    let currentUser = try (self.managedObjectContext!.executeFetchRequest(NSFetchRequest(entityName: "User")) as! [User]).first
+                    print("fetched \(currentUser?.history)")
+                    self.data+=(currentUser?.history)!
+                    self.tableView.reloadData()
+                    
+                } catch {
+                    // We should probably handle the case where this save fails but for now
+                    fatalError("Failed to fetch currentUser: \(error)")
+                }
+        })
+        return self.data
+    }
+
     
 }
